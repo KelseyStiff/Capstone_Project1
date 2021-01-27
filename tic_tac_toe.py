8a6c98f844fbfaee7a4138815d55e5fce67d5811
kelseystiff <kelsey>
2021-01-27 17:37:27 -0600

    fixed winner and end game

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -39,23 +39,12 @@ def update_board(board,move,character):
     new_board = [place.replace(str(move), character) for place in board]
     return new_board
 
-def play_again(player_character):
+def play_again():
     play_again = input('Would you like to play again? (Y/N)').upper
     if play_again == 'Y':
-        player_character = ''
-        main()
+        return True
     elif play_again == 'N':
-        quit()
-
-
-        while True:
-            try:
-                play_again = input('Would you like to play again? (Y/N)').upper
-            except: 
-                print('PLEASE ENTER Y or Yes N for No')
-            else: 
-                if play_again == 'Y':
-                    main()
+        return False
 
 def check_winner(board,moves):
     #possible wins
@@ -95,42 +84,61 @@ def check_winner(board,moves):
 
 
 def main():
-   
-    print('LETS PLAY TIC TAC TOE!')
-    possible_moves = [1,2,3,4,5,6,7,8,9]
-    
-    player_character = assign_player_character()
-    computer_character = assign_computer_character(player_character)
-    player_moves = []
-    computer_moves = []
-    board = ['1 | 2 | 3 ','__________', '4 | 5 | 6','__________', '7 | 8 | 9']
-    for b in board:
-            print(b)
-
-    while player_character:
-
-        player_move = player_turn(possible_moves)
-        player_moves.append(player_move)
-        board = update_board(board,player_move,player_character)
-
-        winner = check_winner(board,player_moves)
-
-        computer_move = computer_turn(possible_moves)
-        computer_moves.append(computer_move)
-        possible_moves.remove(computer_move)
-        board = update_board(board,computer_move,computer_character)
-
-
+    while True:
+        print('LETS PLAY TIC TAC TOE!')
+        possible_moves = [1,2,3,4,5,6,7,8,9]
+        player_character = assign_player_character()
+        computer_character = assign_computer_character(player_character)
+        player_moves = []
+        computer_moves = []
+        board = ['1 | 2 | 3 ','__________', '4 | 5 | 6','__________', '7 | 8 | 9']
         for b in board:
-            print(b)
+                print(b)
 
-        if winner:
-            print('YOU WON!')
-            play_again(player_character)
-        else:
-            'ITS A TIE!'
-    else:
-        print('YOU LOSE')
-        #print('YOU LOSE! OR TIE?')
+        count = 0
+
+        for i in range(10):
+            player_move = player_turn(possible_moves)
+            player_moves.append(player_move)
+            board = update_board(board,player_move,player_character)
+            count += 1
+
+            if possible_moves:
+                computer_move = computer_turn(possible_moves)
+                computer_moves.append(computer_move)
+                possible_moves.remove(computer_move)
+                board = update_board(board,computer_move,computer_character)
+                count += 1
+
+                for b in board:
+                    print(b)
+
+                player_won = check_winner(board,player_moves)
+                print(player_won)
+                computer_won = check_winner(board,computer_moves)
+                print(computer_won)
+
+                if player_won:
+                    print('YOU WON!')
+                    play = play_again()
+                    if play:
+                        main()
+                    else:
+                        exit()
+                elif computer_won:
+                    print('YOU LOSE')
+                    play = play_again()
+                    if play:
+                        main()
+                    else:
+                        exit()
+
+
+            if not possible_moves:             
+                print("It's a Tie!!")
+                play_again()
+                
+
 
 main()
+    
